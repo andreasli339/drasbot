91b65473e615aba5e4334c1bf658c157ee8b32a0
andreasli339 <66297395+andreasli339@users.noreply.github.com>
2021-12-19 23:03:02 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -24,7 +24,7 @@ module.exports = {
       )
       .setColor("BLUE")
       .setDescription(
-        `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=${
+        `You can invite me by clicking [here](https://discord.com/api/oauth2/authorize?client_id=897098618358603806&permissions=8&scope=bot{
           client.botconfig.ClientID
         }&permissions=${
           client.botconfig.Permissions
@@ -50,7 +50,7 @@ module.exports = {
         )
         .setColor("BLUE")
         .setDescription(
-          `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=${
+          `You can invite me by clicking [here](https://discord.com/api/oauth2/authorize?client_id=897098618358603806&permissions=8&scope=bot{
             client.botconfig.ClientID
           }&permissions=${
             client.botconfig.Permissions
